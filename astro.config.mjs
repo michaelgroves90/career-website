b3be9520f33e58c3fc66020734d1618baffaeb3a
Michael Groves <michaeliangroves@gmail.com>
2024-07-14 00:08:05 +0100

    image updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-
 import react from "@astrojs/react";
+
 import image from "@astrojs/image";
 
 // https://astro.build/config
